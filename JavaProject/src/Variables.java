0a54a376764aec1e731c648b529d98bb615f991a
kyll3p <121042283+kyll3p@users.noreply.github.com>
2022-12-20 21:30:03 +0200

    Create Variables.java

---
+++ JavaProject/src/Variables.java
@@ -0,0 +1,29 @@
+public class Variables {
+    public static void main(String[] args) {
+        byte myByte;
+        myByte = 4;
+
+        System.out.println(myByte);
+
+        short myShort = 2343;
+
+        int myInt = 1111111111;
+
+        long muLong = 123123213123213L;
+
+        System.out.println("myShort = " + myShort);
+
+        boolean isTrue = true;
+        boolean isFalse = false;
+
+        char myChar = '\u3453';
+
+        String myName = "sergei";
+
+        System.out.println(myChar);
+
+        System.out.println(myName);
+
+        System.out.println("myInt = " + myInt);
+    }
+}
