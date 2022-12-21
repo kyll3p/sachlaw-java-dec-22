d6929e3ea95342bbbb318599311184f5b4adcf71
kyll3p <121042283+kyll3p@users.noreply.github.com>
2022-12-21 20:47:26 +0200

    Update Home_Task_lesson_2.java

--- JavaProject/src/Home_Task/Home_Task_lesson_2.java
+++ JavaProject/src/Home_Task/Home_Task_lesson_2.java
@@ -2,6 +2,61 @@ package Home_Task;
 
 public class Home_Task_lesson_2 {
     public static void main(String[] args) {
+        byte myByte = -128;
+        byte myByteTwo = 127;
 
+        short myShort = -32768;
+        short myShortTwo = 32767;
+
+        int myInt = -2147483648;
+        int myIntTwo = 2147483647;
+
+        long myLong = -9223372;
+        long myLongTwo = 922337203;
+
+        System.out.println("myByte = " + myByte);
+        System.out.println("myByteTwo = " + myByteTwo);
+
+        System.out.println("myShort = " + myShort);
+        System.out.println("myShortTwo = " + myShortTwo);
+
+        System.out.println("myInt = " + myInt);
+        System.out.println("myIntTwo = " + myIntTwo);
+
+        System.out.println("myLong = " + myLong);
+        System.out.println("myLongTwo = " + myLongTwo);
+
+        float myFloat = 1.2f;
+        float myFloatTwo = 123.1f;
+
+        System.out.println("myFloat = " + myFloat);
+        System.out.println("myFloatTwo = " + myFloatTwo);
+
+        double myDouble = 21.1234;
+        double myDoubleTwo = 456.12312412;
+
+        System.out.println("myDouble = " + myDouble);
+        System.out.println("myDoubleTwo = " + myDoubleTwo);
+
+        boolean isTrue = true;
+        boolean isFalse = false;
+
+        System.out.println("isTrue = " + isTrue);
+        System.out.println("isFalse = " + isFalse);
+
+        char myChar = 0;
+        System.out.println("myChar = " + myChar);
+
+        char myChar2;
+        myChar2 = 6553;
+        System.out.println("myChar2 = " + myChar2);
+
+        char myChar3, myChar4, myChar5;
+        myChar3 = 'a';
+        myChar4 = '\u0054';
+        myChar5 = '5';
+        System.out.println("myChar3 = " + myChar3);
+        System.out.println("myChar4 = " + myChar4);
+        System.out.println("myChar5 = " + myChar5);
     }
 }
