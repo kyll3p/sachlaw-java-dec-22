3e0e34ff13b13bce100d9636d7487b8e1e4f2b29
kyll3p <121042283+kyll3p@users.noreply.github.com>
2022-12-24 20:44:45 +0200

    Create ArithmeticOperations.java

---
+++ JavaProject/src/lesson_3/ArithmeticOperations.java
@@ -0,0 +1,21 @@
+package lesson_3;
+
+public class ArithmeticOperations {
+    public static void main(String[] args) {
+        int a = 15;
+        int b = 6;
+
+        double c = 20;
+        double d = 7;
+
+        double result = (c / d);
+
+        System.out.println("a + b = " + (a + b));
+        System.out.println("a - b = " + (a - b));
+        System.out.println("a * b = " + (a * b));
+        System.out.println("a / b = " + (a / b));
+        System.out.println("a % b = " + (a % b));
+
+        System.out.println("c / d = " + (c / d));
+    }
+}
