68ec0c8861c293a202211c9d3f71715d146ec013
kyll3p <121042283+kyll3p@users.noreply.github.com>
2022-12-24 22:22:33 +0200

    Create IncrementDecrement.java

---
+++ JavaProject/src/lesson_3/IncrementDecrement.java
@@ -0,0 +1,19 @@
+package lesson_3;
+
+public class IncrementDecrement {
+    public static void main(String[] args) {
+        int a = 5;
+        a = a + 5;
+
+        System.out.println("a = " + a);
+
+        a += 11;
+        System.out.println("a = " + a);
+
+        a++;
+        System.out.println("a = " + a);
+
+        a--;
+        System.out.println("a = " + a);
+    }
+}
