166252b9c42d2cea6e1ca75ea66815b0784ce78c
kyll3p <121042283+kyll3p@users.noreply.github.com>
2022-12-25 21:39:32 +0200

    Create Calculator.java

---
+++ JavaProject/src/lesson_3/Calculator.java
@@ -0,0 +1,26 @@
+package lesson_3;
+
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        double firstNumber;
+        double secondNumber;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Введите первое число: ");
+        firstNumber = input.nextDouble();
+
+        System.out.println("Введите второе число: ");
+        secondNumber = input.nextDouble();
+
+        System.out.println("firstNumber = " + firstNumber);
+        System.out.println("secondNumber = " + secondNumber);
+
+        System.out.println(firstNumber + " + " + secondNumber + " = " + (firstNumber + secondNumber));
+        System.out.println(firstNumber + " - " + secondNumber + " = " + (firstNumber - secondNumber));
+        System.out.println(firstNumber + " x " + secondNumber + " = " + (firstNumber * secondNumber));
+        System.out.println(firstNumber + " : " + secondNumber + " = " + (firstNumber / secondNumber));
+    }
+}
