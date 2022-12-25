9589613b56d26b097d91bd4d7e3c8b6a5360badf
kyll3p <121042283+kyll3p@users.noreply.github.com>
2022-12-25 21:17:11 +0200

    Create ComparisonOperators.java

---
+++ JavaProject/src/lesson_3/ComparisonOperators.java
@@ -0,0 +1,13 @@
+package lesson_3;
+
+public class ComparisonOperators {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 6;
+        int c = 6;
+
+        System.out.println("a == b " + (a == b));
+        System.out.println("b == c " + (b == c));
+        System.out.println("a <= b " + (a <= b));
+    }
+}
