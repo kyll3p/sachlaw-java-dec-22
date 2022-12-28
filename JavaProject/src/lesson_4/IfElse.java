379f37ed6a9ece68a586daab463d4f107a640870
kyll3p <121042283+kyll3p@users.noreply.github.com>
2022-12-28 22:34:40 +0200

    Lesson 3_01 second commit

--- JavaProject/src/lesson_4/IfElse.java
+++ JavaProject/src/lesson_4/IfElse.java
@@ -4,20 +4,26 @@ import java.util.Scanner;
 
 public class IfElse {
     public static void main(String[] args) {
-        String name = "Sergei";
-        String anotherName = "Sergei";
-        boolean areEqual = name.equals(anotherName);
-
-        System.out.println("areEqual = " + areEqual);
-
-        Scanner input = new Scanner(System.in);
-
+        /*Scanner input = new Scanner(System.in);
         System.out.println("Please enter your age... ");
         int age = input.nextInt();
         if (age >= 18) {
             System.out.println("You are adult");
         }   else {
             System.out.println("You are young");
+        }*/
+
+        int number;
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter number: ");
+        number = input.nextInt();
+
+
+        if(number % 2 == 0) {
+            System.out.println("Number " + number + " is even");
+        } else {
+            System.out.println("Number " + number + " is odd");
         }
     }
 }
