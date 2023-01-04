f91c2a70a85ab2befea9f0226434ba4491f7c8d9
kyll3p <121042283+kyll3p@users.noreply.github.com>
2023-01-04 12:38:34 +0200

    Create RandomValuesArray.java

---
+++ JavaProject/src/lesson_6/RandomValuesArray.java
@@ -0,0 +1,19 @@
+package lesson_6;
+
+import java.util.Random;
+
+public class RandomValuesArray {
+    public static void main(String[] args) {
+        Random random = new Random();
+
+        int array[] = new int[100];
+
+        for (int i = 0; i < array.length; i++) {
+            array[i] = random.nextInt();
+        }
+
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i] + " ");
+        }
+    }
+}
